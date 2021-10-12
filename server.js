dab2a2d716feeabd5781a3b8578796cb8aebf2b2
jayTee <tamjidfb@gmail.com>
2021-10-12 22:40:30 +0600

    server.js updated

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = 5555;
 
 app.use(bodyParser.json());
 
+// Routes
+app.get("/", (req, res) => {
+  res.send('Welcome to the "tackeon" app\'s Express.js powered api.');
+});
+
 // Initiate server
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
