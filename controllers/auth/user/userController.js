de0edb8e7c7ab3cf25b11c32646b3d5b05491468
Tam Jid <tamjid.mdh@gmail.com>
2021-10-13 07:12:42 +0600

    db conneted

--- controllers/auth/user/userController.js
+++ controllers/auth/user/userController.js
@@ -1,46 +1,38 @@
+import CryptoJS from "crypto-js";
+import User from "../../../models/auth/User.js";
+
 let users = [];
 
 export const index = (req, res) => {
   res.send(users);
 };
 
-export const store = (req, res) => {
-  res.send("store");
+export const store = async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.JWT_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 export const show = (req, res) => {
-  // const { id } = req.params;
-  // const getUesr = users.find((user) => user.id === id);
-  // res.send(getUesr);
   res.send("show");
 };
 
 export const update = (req, res) => {
-  // const { id } = req.params;
-
-  // const { firstName, lastName, age } = req.body;
-
-  // const user = users.find((user) => user.id === id);
-
-  // if (firstName) {
-  //   user.firstName = firstName;
-  // }
-  // if (lastName) {
-  //   user.lastName = lastName;
-  // }
-  // if (age) {
-  //   user.age = age;
-  // }
-
-  // res.send(`User with the id ${user.id} updated`);
   res.send("update");
 };
 
 export const destroy = (req, res) => {
-  // const { id } = req.params;
-
-  // users = users.filter((user) => user.id !== id);
-
-  // res.send(`User with id ${id} deleted`);
   res.send("destroy");
 };
