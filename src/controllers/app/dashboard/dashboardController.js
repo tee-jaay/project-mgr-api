75ae186282e9cecd2d3629a4192174d264d943f8
jayTee <tamjidfb@gmail.com>
2021-11-11 02:15:22 +0600

    recent projects api

--- src/controllers/app/dashboard/dashboardController.js
+++ src/controllers/app/dashboard/dashboardController.js
@@ -2,6 +2,7 @@ import moment from "moment";
 import Task from "../../../models/app/Task.js";
 import Issue from "../../../models/app/Issue.js";
 import Meeting from "../../../models/app/Meeting.js";
+import Project from "../../../models/app/Project.js";
 
 export const index = async (req, res) => {
   let data = null;
@@ -9,17 +10,23 @@ export const index = async (req, res) => {
   let allIssuesCount = null;
   let allMeetingsCount = null;
   let today = null;
+  let recentProjects = null;
   try {
     today = await moment(new Date()).format("MMMM Do YYYY");
     allTasksCount = await Task.countDocuments({});
     allIssuesCount = await Issue.countDocuments({});
     allMeetingsCount = await Meeting.countDocuments({});
+    recentProjects = await Project.find(
+      {},
+      { id: 1, title: 1, status: 1 }
+    ).limit(5);
     data = [
       {
         today: today,
         allTasksCount: allTasksCount,
         allIssuesCount: allIssuesCount,
         allMeetingsCount: allMeetingsCount,
+        recentProjects,
       },
     ][0];
     res.status(200).json(data);
