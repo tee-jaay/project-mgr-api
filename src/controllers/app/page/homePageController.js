fb3c00dcb36ebf4eb165674fd66d8c91e798e855
jayTee <tamjidfb@gmail.com>
2022-01-04 18:02:43 +0600

    homepage components add draft

--- src/controllers/app/page/homePageController.js
+++ src/controllers/app/page/homePageController.js
@@ -3,50 +3,95 @@ import HomePage from "../../../models/app/HomePage.model.js";
 import colors from "colors";
 import { uploadFileToCloudinary } from "../../../services/fileUpload.js";
 
-export const store = async (req, res) => {
-  console.log(req.body);
-  const { image } = req.body;
-
-  const newHomepage = new HomePage({
-    id: uuidv4(),
-    logo: image,
-  });
+export const store = async (req, res) => {};
+export const show = async (req, res) => {
   try {
-    const savedHomepage = await newHomepage.save();
-    res.status(201).json(savedHomepage);
+    const homePage = await HomePage.findOne();
+
+    res.status(200).json(homePage);
   } catch (error) {
     res.status(500).json(error);
   }
 };
-export const show = async (req, res) => {
-  res.status(200).json("home show");
-};
 export const update = async (req, res) => {
+  console.log(colors.blue(req.body.feature));
   const { about } = req.body;
   const homePageCount = await HomePage.countDocuments();
   try {
-    const result = await uploadFileToCloudinary(
-      req.file.path,
-      "settings/homepage"
-    );
+    // const result = await uploadFileToCloudinary(
+    //   req.file.path,
+    //   "settings/homepage"
+    // );
 
     if (homePageCount === 0) {
+      const result = await uploadFileToCloudinary(
+        req.file.path,
+        "settings/homepage"
+      );
       const homePageObj = new HomePage({
         id: uuidv4(),
         logo: result.secure_url,
         about: about,
+        features: [...features, feature],
       });
       const savedObj = await homePageObj.save();
       res.status(201).json(savedObj);
     } else {
-      const updated = await HomePage.findOneAndUpdate({
-        about: about,
-      });
-      updated.save();
+      if (req.file) {
+        const result = await uploadFileToCloudinary(
+          req.file.path,
+          "settings/homepage"
+        );
+
+        const updated = await HomePage.findOneAndUpdate({
+          logo: result.secure_url,
+          about: about,
+        });
+        updated.save();
+      } else {
+        const updated = await HomePage.findOneAndUpdate({
+          // logo: "result.secure_url",
+          about: about,
+          features: [...features, feature],
+        });
+        updated.save();
+      }
+
       const homePageObj = await HomePage.find().sort({ $natural: -1 }).limit(1);
       res.status(202).json(homePageObj);
     }
   } catch (error) {
     res.status(500).json(error);
   }
+};
+
+export const featureAdd = async (req, res) => {
+  const { feature } = req.body;
+
+  try {
+    const homePageObj = await HomePage.find();
+    console.log(colors.grey(homePageObj[0].features));
+    await homePageObj[0].features.unshift({
+      title: feature.title,
+      image: feature.image,
+      description: feature.description,
+    });
+    const savedObj = await homePageObj[0].save();
+    res.status(201).json(savedObj);
+  } catch (error) {
+    console.log(colors.red(error));
+    res.status(500).json(error);
+  }
+};
+
+export const techAdd = async (req, res) => {
+  console.log(req.body);
+};
+
+export const serverAdd = async (req, res) => {
+  console.log(req.body);
+};
+
+export const systemAdd = async (req, res) => {
+  console.log(req.body);
 };
--- src/routes/pages/homePage.route.js
+++ src/routes/pages/homePage.route.js
@@ -3,6 +3,10 @@ import {
   store,
   show,
   update,
+  featureAdd,
+  techAdd,
+  serverAdd,
+  systemAdd,
 } from "../../controllers/app/page/homePageController.js";
 import { upload } from "../../services/fileUpload.js";
 
@@ -14,5 +18,13 @@ router.post("/", store);
 router.get("/", show);
 // Update
 router.patch("/update", upload.single("image"), update);
+// Feature
+router.patch("/feature/add", featureAdd);
+// Tech
+router.patch("/tech/add", techAdd);
+// Server
+router.patch("/server/add", serverAdd);
+// System
+router.patch("/system/add", systemAdd);
 
 export default router;
