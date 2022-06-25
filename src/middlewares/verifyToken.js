d4529c350d3788e634bcfc264414e506f7c13bf6
Tam Jid <tamjid.mdh@gmail.com>
2022-06-25 22:56:46 +0600

    project store authorization middleware

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,25 +1,8 @@
-import jwt from "jsonwebtoken";
-
-export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) {
-        res.status(403).json("Token is not valid!")
-      } else {
-        req.user = user;
-        next();
-      };
-    });
-  } else {
-    return res.status(401).json("You are not authenticated!");
-  }
-};
+import { verifyLogin } from "../app/middleware.js";
 
 export const verifyTokenAndAuthorization = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.id === req.params.id || req.user.role === "admin") {
+  verifyLogin(req, res, () => {
+    if (req.user.id === req.params.id || req.user.role.type === "admin") {
       next();
     } else {
       res.status(403).json("Unauthorized access!");
@@ -28,8 +11,8 @@ export const verifyTokenAndAuthorization = (req, res, next) => {
 };
 
 export const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role === "admin") {
+  verifyLogin(req, res, () => {
+    if (req.user.role.type === "admin") {
       next();
     } else {
       res.status(403).json("Unauthorized access!");
